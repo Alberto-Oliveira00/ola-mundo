c863c781aa218eb56380bcaa9dfdf8cd10caf567
Alberto Oliveira <alberto.olv00@gmail.com>
2025-03-24 11:21:04 -0300

    Banner

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,5 +1,34 @@
 import styles from "./Banner.module.css"
+import circuloColorido from "assets/circulo_colorido.png"
+import minhaFoto from "assets/imagem-alberto2.jpg"
 
 export default function Banner() {
-    return "Banner"
+    return (
+        <div className={styles.banner}>
+            <div className={styles.apresentacao}>
+                <h1 className={styles.titulo}>
+                    Olá, Mundo!
+                </h1>
+
+                <p className={styles.paragrafo}>
+                    Boas vindas ao meu espaço pessoal! Eu sou Alberto Oliveira, desenvolvedor Front-End. Aqui compartilho vários conhecimentos, espero que aprenda algo novo :)
+                </p>
+            </div>
+
+            <div className={styles.imagens}>
+                <img
+                    alt=""
+                    className={styles.circuloColorido}
+                    src={circuloColorido}
+                    aria-hidden={true}
+                />
+
+                <img 
+                    alt="Foto Alberto"
+                    className={styles.minhaFoto}
+                    src={minhaFoto}
+                />
+            </div>
+        </div>
+    )
 } 
